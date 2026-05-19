ea6e8ea23fdda85fe148fe713d5b217903c0c0e4
Hisham <hisham@globalsurf.ae>
2026-05-19 11:20:35 +0530

    feat: careers page created

---
+++ app/components/client/careers/data.ts
@@ -0,0 +1,73 @@
+export const bannerData = {
+  title: "Join Neuro Vanta",
+  image: "/assets/images/careers/banner.jpg",
+};
+
+export const careerIntro = {
+  title: "Why Work With Us",
+  description:
+    "Be part of a forward-thinking company shaping the future of wellness and human performance.",
+  bgImage: "/assets/images/careers/career-intro.jpg",
+};
+
+
+export const careerCulture = {
+  title: "Culture",
+  description: "We foster innovation, collaboration, and growth.",
+  images: [
+    "/assets/images/careers/culture/1.jpg",
+    "/assets/images/careers/culture/2.jpg",
+    "/assets/images/careers/culture/3.jpg",
+    "/assets/images/careers/culture/4.jpg",
+    "/assets/images/careers/culture/5.jpg",
+  ],
+};
+
+
+export const openPositionsData = {
+  title: "OPEN POSITIONS",
+  subtitle: "Or Contact With",
+  mail: "Info@neurovanta.com",
+  positions: [
+    {
+      role: "Open Roles",
+      title: "Job Designation Name",
+      time: "Full Time",
+      location: "Location",
+      btn: {
+        label: "APPLY JOB",
+        href: "/careers/apply",
+      },
+    },
+    {
+      role: "Open Roles",
+      title: "Job Designation Name",
+      time: "Full Time",
+      location: "Location",
+      btn: {
+        label: "APPLY JOB",
+        href: "/careers/apply",
+      },
+    },
+    {
+      role: "Open Roles",
+      title: "Job Designation Name",
+      time: "Full Time",
+      location: "Location",
+      btn: {
+        label: "APPLY JOB",
+        href: "/careers/apply",
+      },
+    },
+    {
+      role: "Open Roles",
+      title: "Job Designation Name",
+      time: "Full Time",
+      location: "Location",
+      btn: {
+        label: "APPLY JOB",
+        href: "/careers/apply",
+      },
+    },
+  ],
+};
